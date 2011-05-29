f7f0877bbd55bcddbab1a2acd4cb2c9dd5e0001e
Nelson Morris <nmorris@nelsonmorris.net>
2011-05-29 00:01:53 -0500

    Fix misuse of is in some tests

--- test/calculator/test/core.clj
+++ test/calculator/test/core.clj
@@ -12,8 +12,8 @@
   (is (= 4 (calculate "2 + 2"))))
 
 (deftest multiplication
-  (is (= 16 (calculate "4 * 4"))
-      (= 6 (calculate "1 * 6"))))
+  (is (= 16 (calculate "4 * 4")))
+  (is (= 6 (calculate "1 * 6"))))
 
 (deftest division
   (is (= 1 (calculate "4 / 4")))
@@ -36,11 +36,11 @@
   (is (= "Error at character 1: expected number or ( ; got +" (calculate "+")))
   (is (= "Error at character 1: expected number or ( ; got eol" (calculate "")))
   (is (= "Error at character 1: expected number or ( ; got x" (calculate "x")))
-  (is (= "Error at character 3: expected operator or eol ; got )")
-      (calculate "1 + 3)"))
-  (is (= "Error at character 3: expected operator or eol ; got 3")
-      (calculate "1 3"))
-  (is (= "Error at character 3: expected operator or eol ; got (")
-      (calculate "1 ("))
+  (is (= "Error at character 3: expected operator or eol ; got )"
+         (calculate "1 + 3)")))
+  (is (= "Error at character 3: expected operator or eol ; got 3"
+         (calculate "1 3")))
+  (is (= "Error at character 3: expected operator or eol ; got ("
+         (calculate "1 (")))
   (is (= "Error at character 5: expected number or ( ; got +"
          (calculate "1 + +"))))
